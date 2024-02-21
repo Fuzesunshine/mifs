892245254bd19c66f32a9ca8e23eb9026488e836
Fuzesunshine <178880457@qq.com>
2024-02-21 16:42:27 +0800

    debug nan but failed

--- mifs/mi.py
+++ mifs/mi.py
@@ -141,46 +141,6 @@ def _nearest_distances(X, k=1):
     return d[:, -1]
 
 
-# def _entropy(X, k=1):
-#     """
-#     Returns the entropy of the X.
-#
-#     Written by Gael Varoquaux:
-#     https://gist.github.com/GaelVaroquaux/ead9898bd3c973c40429
-#
-#     Parameters
-#     ----------
-#     X : array-like, shape (n_samples, n_features)
-#         The data the entropy of which is computed
-#     k : int, optional
-#         number of nearest neighbors for density estimation
-#
-#     References
-#     ----------
-#     Kozachenko, L. F. & Leonenko, N. N. 1987 Sample estimate of entropy
-#     of a random vector. Probl. Inf. Transm. 23, 95-101.
-#     See also: Evans, D. 2008 A computationally efficient estimator for
-#     mutual information, Proc. R. Soc. A 464 (2093), 1203-1215.
-#     and:
-#
-#     Kraskov A, Stogbauer H, Grassberger P. (2004). Estimating mutual
-#     information. Phys Rev E 69(6 Pt 2):066138.
-#
-#     F. Perez-Cruz, (2008). Estimation of Information Theoretic Measures
-#     for Continuous Random Variables. Advances in Neural Information
-#     Processing Systems 21 (NIPS). Vancouver (Canada), December.
-#     return d*mean(log(r))+log(volume_unit_ball)+log(n-1)-log(k)
-#
-#     """
-#
-#     # Distance to kth nearest neighbor
-#     r = _nearest_distances(X, k)
-#     n, d = X.shape
-#     volume_unit_ball = (np.pi ** (.5 * d)) / gamma(.5 * d + 1)
-#     return (d * np.mean(np.log(r + np.finfo(X.dtype).eps)) +
-#             np.log(volume_unit_ball) + psi(n) - psi(k))
-
-
 def _entropy(X, k=1):
     """
     Returns the entropy of the X.
@@ -218,4 +178,4 @@ def _entropy(X, k=1):
     n, d = X.shape
     volume_unit_ball = (np.pi ** (.5 * d)) / gamma(.5 * d + 1)
     return (d * np.mean(np.log(r + np.finfo(X.dtype).eps)) +
-            np.log(volume_unit_ball) + digamma(n) - digamma(k))
+            np.log(volume_unit_ball) + psi(n) - psi(k))
